655ad46d9221e3b06de2da29df78b58f91a09f65
SemenZvehintsev <kvadratniibomzh@gmail.com>
2022-11-01 00:33:57 +0200

    added 1 and 2 tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+
+class Gallery {
+    constructor(images) {
+        this.images = images;
+        this.fullGallery = document.querySelector('.gallery');
+    }
+
+    start() {
+        this.createGallery()
+    }
+
+    createGallery() {
+        this.images.forEach(({preview, original, description}) => {
+        this.fullGallery.insertAdjacentHTML("afterbegin",
+        `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`);})
+    }
+}
+
+new Gallery(galleryItems).start();
+
+let lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,39 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+class VimeoPlayer {
+
+    iframe = document.querySelector('iframe');
+    player = new Player(this.iframe);
+    timeToPlay = localStorage.getItem("videoplayer-current-time");
+    
+
+    start() {
+        this.startTime()
+        this.newPlayTime()
+    }
+
+    startTime() {this.player.on('timeupdate', throttle(this.playOn, 1000))}
+    
+    playOn({seconds}) {
+        const currentTime = seconds;
+        localStorage.setItem("videoplayer-current-time", currentTime);
+    }
+    
+    newPlayTime() {
+        this.player.setCurrentTime(this.timeToPlay).then(() => {    
+            this.player.play()
+        }).catch(function(error) {    
+            switch (error.name) {        
+                case 'RangeError':         
+                console.log('the time was less than 0 or greater than the video’s duration')   
+                break;
+                
+                default:
+                console.log('some other error occurred')
+                break;
+            }
+        });}
+}
+
+new VimeoPlayer().start();
