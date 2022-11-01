d379cac458d4ac85880d90ea9589149deb0eb051
SemenZvehintsev <kvadratniibomzh@gmail.com>
2022-11-01 20:29:07 +0200

    added 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,64 +1,43 @@
 import throttle from 'lodash.throttle';
 
-
 const form = document.querySelector('form')
-form.addEventListener('input', handleSubmit)
-
 const input = document.querySelector('input')
 const textarea = document.querySelector('textarea')
 
+const inputValues = {
+    email: '',
+    message: ''
+}
 
+form.addEventListener('submit', clearAll)
+input.addEventListener('input', throttle(typeEmail, 500))
+textarea.addEventListener('input', throttle(typeMessage, 500))
+window.addEventListener('load', pageLoad)
 
-function handleSubmit(event) {
-    let inputValues = {
-        email: '',
-        message: ''
-    }
-  
-    if (event.target === input) {
-        inputValues = JSON.parse(localStorage.getItem("feedback-form-state"))
-        inputEmail = event.target.value;
-        inputValues.email = inputEmail;
-    } else {
-        inputValues = JSON.parse(localStorage.getItem("feedback-form-state"))
-        inputMessage = event.target.value;
-        inputValues.message = inputMessage;
-    }            
-        localStorage.setItem("feedback-form-state", JSON.stringify(inputValues));
-        console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
-  }
+function typeEmail(event) {
+    inputValues.email = event.currentTarget.value;
+    localStorageRenew()
+}
 
+function typeMessage(event) {
+    inputValues.message = event.currentTarget.value;
+    localStorageRenew()
+}
 
-// class inputForm {
+function localStorageRenew() {
+    localStorage.setItem("feedback-form-state", JSON.stringify(inputValues));
+}
 
-//     form = document.querySelector('form');
-//     input = document.querySelector('input');
-//     textarea = document.querySelector('textarea');
-//     inputValues = {
-//         email: '',
-//         message: ''
-//     }
+function pageLoad() {
+    lastTyped = JSON.parse(localStorage.getItem("feedback-form-state"));
+    if (lastTyped !== null) {
+        input.value = lastTyped.email;
+        textarea.value = lastTyped.message;}   
+}
 
-//     start(){
-//         this.inputFull()
-//         console.log(this.inputValues)
-
-//     }
-
-//     inputFull() {this.form.addEventListener('input', this.importText)}
-
-//     importText(event) {
-//         if (event.target === this.input) {
-//             this.inputValues.email = event.target.value
-//             console.log(this.inputValues)
-
-//         } if (event.target === this.textarea) {
-//             this.inputValues.message = event.target.value
-//             console.log(this.inputValues)
-
-//         }
-//     }
-    
-// }
-
-// new inputForm().start()
+function clearAll(event) {
+    event.preventDefault();
+    console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
+    localStorage.removeItem("feedback-form-state");
+    event.currentTarget.reset();
+}
