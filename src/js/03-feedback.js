e2154221fb69de3cbfd1d7a4b0f11970f7e66439
SemenZvehintsev <kvadratniibomzh@gmail.com>
2022-11-01 13:54:56 +0200

    upd 03 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,4 +2,63 @@ import throttle from 'lodash.throttle';
 
 
 const form = document.querySelector('form')
-form.addEventListener('input', (event) => {console.log(event.target.value)})
+form.addEventListener('input', handleSubmit)
+
+const input = document.querySelector('input')
+const textarea = document.querySelector('textarea')
+
+
+
+function handleSubmit(event) {
+    let inputValues = {
+        email: '',
+        message: ''
+    }
+  
+    if (event.target === input) {
+        inputValues = JSON.parse(localStorage.getItem("feedback-form-state"))
+        inputEmail = event.target.value;
+        inputValues.email = inputEmail;
+    } else {
+        inputValues = JSON.parse(localStorage.getItem("feedback-form-state"))
+        inputMessage = event.target.value;
+        inputValues.message = inputMessage;
+    }            
+        localStorage.setItem("feedback-form-state", JSON.stringify(inputValues));
+        console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
+  }
+
+
+// class inputForm {
+
+//     form = document.querySelector('form');
+//     input = document.querySelector('input');
+//     textarea = document.querySelector('textarea');
+//     inputValues = {
+//         email: '',
+//         message: ''
+//     }
+
+//     start(){
+//         this.inputFull()
+//         console.log(this.inputValues)
+
+//     }
+
+//     inputFull() {this.form.addEventListener('input', this.importText)}
+
+//     importText(event) {
+//         if (event.target === this.input) {
+//             this.inputValues.email = event.target.value
+//             console.log(this.inputValues)
+
+//         } if (event.target === this.textarea) {
+//             this.inputValues.message = event.target.value
+//             console.log(this.inputValues)
+
+//         }
+//     }
+    
+// }
+
+// new inputForm().start()
